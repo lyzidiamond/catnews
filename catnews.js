c645e7b5a24ce08b88e69abe26f51e07d9939a1b
Lyzi Diamond <lyzidiamond@gmail.com>
2014-02-05 02:30:43 -0500

    add article date

--- catnews.js
+++ catnews.js
@@ -7,7 +7,7 @@ var cat = L.icon({
 });
 
 function onEachFeature(feature, layer) {
-  var popupContent = "<a href='" + feature.properties.article_link + "'>" + feature.properties.article_headline +"</a><br>" + feature.properties.article_desc;
+  var popupContent = "<a href='" + feature.properties.article_link + "'>" + feature.properties.article_headline +"</a><br><em>" + feature.properties.article_date + "</em><br>" + feature.properties.article_desc;
   layer.bindPopup(popupContent);
   layer.on('click', function(e) {
     map.fitBounds(feature.getBounds(), {reset:true});
