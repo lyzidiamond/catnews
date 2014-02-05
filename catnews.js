144db186fa4792aee80066c0b51745778b2966b7
Lyzi Diamond <lyzidiamond@gmail.com>
2014-02-05 00:47:00 -0500

    add js file, update with map tile id

---
+++ catnews.js
@@ -0,0 +1,17 @@
+// initialize map
+var map = L.mapbox.map('cat-map', 'codeforamerica.h6ihl7do').setView([37.770986,-122.446457], 13);
+
+function onEachFeature(feature, layer) {
+  var popupContent = "<strong>< a href='" + feature.properties.article_link + "'>" + feature.properties.article_headline +"</a></strong><br>" + feature.properties.article_desc;
+  layer.bindPopup(popupContent);
+};
+
+var locations;
+
+$.getJSON('catnews.geojson', function(data) {
+  var locations = L.geoJson(data, {
+    onEachFeature: onEachFeature
+  });
+  locations.addTo(map);
+  map.fitBounds(locations.getBounds(), {reset: true});
+});
