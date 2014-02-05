88b7091e29df764b7f4445cd9642d81c3dc5487b
Lyzi Diamond <lyzidiamond@gmail.com>
2014-02-05 00:59:18 -0500

    add custom cat icon

--- catnews.js
+++ catnews.js
@@ -1,6 +1,11 @@
 // initialize map
 var map = L.mapbox.map('cat-map', 'codeforamerica.h6ihl7do').setView([37.770986,-122.446457], 13);
 
+var cat = L.icon({
+  iconUrl: '/cat.png',
+  iconSize: [25, 25]
+});
+
 function onEachFeature(feature, layer) {
   var popupContent = "<strong>< a href='" + feature.properties.article_link + "'>" + feature.properties.article_headline +"</a></strong><br>" + feature.properties.article_desc;
   layer.bindPopup(popupContent);
@@ -10,6 +15,9 @@ var locations;
 
 $.getJSON('catnews.geojson', function(data) {
   var locations = L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      return L.marker(latlng, {icon: cat});
+    },
     onEachFeature: onEachFeature
   });
   locations.addTo(map);
